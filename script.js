95f8b9078e3550a40a2da5855c425f032523a785
Sakshi Jain <62291788+im-sakshijain@users.noreply.github.com>
2020-12-10 18:29:22 +0530

    import js

---
+++ script.js
@@ -0,0 +1,11 @@
+//input
+//var username=prompt("Give Me Your Name")
+//processing
+//var welcomeMessage="this script works! "+username
+//script
+//alert(welcomeMessage);
+var btnTranslate= document.querySelector("#btn-translate");
+//console.log(btnTranslate)
+btnTranslate.addEventListener("click",function clickEventHandler(){
+    console.log("clicked!")
+})
